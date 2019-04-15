ad2ff4197075b90bcd973bd3e257f25ff36bf60e
Chae-Yeun Park <chae.yeun.park@gmail.com>
2019-04-16 00:45:43 +0200

    Trait Model added

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,52 @@
 use std::vec;
 
 
-mod Ising2D {
-    pub struct Model {
+mod Ising {
+
+    pub trait Model {
+        fn neighbors(&self, n: u32) -> Vec<u32>;
+        fn size(&self) -> u32;
+    }
+
+    pub struct Ising2D {
         pub n1: u32,
         pub n2: u32,
     }
-    pub struct WolffSampler<'a> {
-        model: &'a Model,
+    pub struct WolffSampler<'a, T: Model> {
+        model: &'a T,
         beta: f64,
         conf: Vec<i8>,
     }
 
-    impl Model {
-        pub fn size(&self) -> u32 {
+    impl Model for Ising2D{
+        fn size(&self) -> u32 {
             self.n1 * self.n2
+        }
+        fn neighbors(&self, n : u32) -> Vec<u32> {
+            let mut res : Vec<u32> = Vec::new();
+            res.push(n + self.n1);
+            if n > self.n1 {
+                res.push(n - self.n1);
+            }
+            res.push(n + 1);
+            if n > 1 {
+                res.push(n - 1);
+            }
+            
+            res.retain(|&i| i < self.size());
+            res
         }
     }
 
-    impl<'a> WolffSampler<'a> {
-        pub fn new (model: &'a Model, beta: f64) -> WolffSampler <'a> {
+    impl<'a, T: Model> WolffSampler<'a, T> {
+        pub fn new (model: &'a T, beta: f64) -> WolffSampler <'a, T> {
             use rand::distributions::Uniform;
             use rand::prelude::*;
 
             let dist = Uniform::from(0..2);
             let mut rng = rand::thread_rng();
 
-            let n = (model.n1*model.n2) as usize;
+            let n = model.size() as usize;
             let mut conf : Vec<i8> = Vec::with_capacity(n);
 
             for _i in 0..n {
@@ -47,21 +67,6 @@ mod Ising2D {
         pub fn getConf(&self) -> &Vec<i8> {
             & self.conf
         }
-        
-        fn neighbors(&self, n : u32) -> Vec<u32> {
-            let mut res : Vec<u32> = Vec::new();
-            res.push(n + self.model.n1);
-            if n > self.model.n1 {
-                res.push(n - self.model.n1);
-            }
-            res.push(n + 1);
-            if n > 1 {
-                res.push(n - 1);
-            }
-            
-            res.retain(|&i| i < self.size());
-            res
-        }
 
         pub fn sweep (&mut self) {
             use std::collections::VecDeque;
@@ -72,7 +77,7 @@ mod Ising2D {
             let mut queue: VecDeque<u32> = VecDeque::new();
 
 
-            let n = self.model.n1*self.model.n2;
+            let n = self.model.size();
             let idx_dist = Uniform::from(0..n);
             let p_dist : Uniform<f64> = Uniform::from(0.0..1.0);
             let mut rng = rand::thread_rng();
@@ -85,7 +90,7 @@ mod Ising2D {
             
             while let Some(k) = queue.pop_front() {
                 cluster.push(k);
-                for x in self.neighbors(k) {
+                for x in self.model.neighbors(k) {
                     if self.conf[k as usize] == self.conf[x as usize] {
                         //push x with probability p = 1-exp(-2*beta)
                         if p_dist.sample(&mut rng) < prob
@@ -104,13 +109,13 @@ mod Ising2D {
 }
 
 fn main() {
-    use Ising2D;
-    let ising2D = Ising2D::Model {
+    use Ising;
+    let ising2D = Ising::Ising2D {
         n1: 5,
         n2: 5,
     };
 
-    let mut ws = Ising2D::WolffSampler::new(&ising2D, 0.1);
+    let mut ws = Ising::WolffSampler::new(&ising2D, 0.1);
 
     println!("{:?}", ws.getConf());
     ws.sweep();

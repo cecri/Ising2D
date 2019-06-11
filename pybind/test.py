c3f516c5ee14bcd7dfceca46ed2221d72c24e579
Chae-Yeun Park <cypark@dialog.thp.uni-koeln.de>
2019-06-11 17:34:50 +0200

    c++ port updated

--- pybind/test.py
+++ pybind/test.py
@@ -1,11 +1,23 @@
 import Ising2D
+import numpy as np
 
-model = Ising2D.Ising2D(4,5)
-print(model.neighbors(0))
-sampler = Ising2D.WolffSampler(model, 100.0)
-sampler.set_seed(2)
-sampler.randomize_conf()
-for i in range(100):
-    sampler.sweep()
-    print(sampler.conf)
+model = Ising2D.Ising2D(10,10)
+
+for beta in np.linspace(0.05, 1.0, 20):
+    sampler = Ising2D.WolffSampler(model, beta)
+    sampler.set_seed(2)
+    sampler.randomize_conf()
+
+    #thermalizing
+    for i in range(100):
+        sampler.sweep()
+
+    #sampling
+    energies = []
+    for i in range(5000):
+        sampler.sweep()
+        energies.append(model.energy(sampler.conf))
+
+    print("{}\t{}".format(beta, np.mean(energies)/model.size))
+
 
